22a56517a6affd8172074fd5f632df7cd0c0ed20
Mriganka Patra <mriganka9432@gmail.com>
2024-07-13 05:48:15 -0400

    modify compile.sh

--- compile.sh
+++ compile.sh
@@ -1,23 +1,23 @@
 cd geco/src
-cp Makefile.linux Makefile1.linux
+cp Makefile.linux Makefile2.linux
 mv Makefile2.linux Makefile
 make clean
 make
 
 cd ../../geco2/src
-cp Makefile.linux Makefile1.linux
+cp Makefile.linux Makefile2.linux
 mv Makefile2.linux Makefile
 make clean
 make
 
 cd ../../geco3/src
-cp Makefile.linux Makefile1.linux
+cp Makefile.linux Makefile2.linux
 mv Makefile2.linux Makefile
 make clean
 make
 
 cd ../../jarvis/src
-cp Makefile.linux Makefile1.linux
+cp Makefile.linux Makefile2.linux
 mv Makefile2.linux Makefile
 make clean
 make
